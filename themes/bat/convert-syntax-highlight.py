4cbe1139f6db3b7ad4b90cb4548101252eda7030
noidilin <linganinja.0120@gmail.com>
2024-07-27 22:09:24 +0800

    feat: bat theme converter

---
+++ themes/bat/convert-syntax-highlight.py
@@ -0,0 +1,78 @@
+import json
+import plistlib
+import os
+import re
+
+
+# Function to remove comments from a JSON string
+def clean_json(json_str):
+    # Remove specific line with "$schema"
+    json_str = re.sub(r'"\$schema":.*', "", json_str)
+    # Remove // comments
+    json_str = re.sub(r"//.*", "", json_str)
+    # Remove /* */ comments
+    json_str = re.sub(r"/\*[\s\S]*?\*/", "", json_str)
+    return json_str
+
+
+# Define the global settings
+global_settings = {
+    # ref: https://www.sublimetext.com/docs/color_schemes_tmtheme.html#global_settings
+    "settings": {
+        "background": "#191919",
+        "foreground": "#b3b3b3",
+        "caret": "#eaeaea",
+        "lineHighlight": "#222222",  # bg for the current line
+        "misspelling": "#af5f5f",  # color to use for the underline drawn under misspelled words
+        "accent": "#dad5c8",  # color made available for use by the theme
+        "selection": "#303030",  # bg of selected text
+        "inactiveSelection": "#242424",  # not currently focused selection
+        "gutterForeground": "#474747",  # line number
+        "findHighlight": "#474747",  # bg color for match result
+        "guide": "#242424",  # indent guide line
+        "activeGuide": "#5d5d5d",  # indent guide line for current caret location
+        "stackGuide": "#353535",  # parent indent guide line for current caret location
+        "bracketsOptions": "foreground",  # next/ previous brackets style
+        "bracketsForeground": "#353535",  # next/ previous brackets color
+        "bracketContentsOptions": "underline foreground",  # surround bracket style
+        "bracketContentsForeground": "#5d5d5d",  # surround bracket color
+        "tagsOptions": "underline foreground",  # surround tags style
+        "tagsForeground": "#5d5d5d",  # surround tags color
+    }
+}
+
+# Create the root dictionary for .tmTheme
+theme_plist = {  # the file structure of the .tmTheme
+    "name": "Color Fatigue",
+    "semanticClass": "theme.dark.color-fatigue",
+    "colorSpaceName": "sRGB",
+    "settings": [global_settings],  # the convert settings will be appended in here
+}
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the path to the vscode_theme.json file in the parent directory
+parent_dir = os.path.dirname(script_dir)
+json_file_path = os.path.join(parent_dir, "color-fatigue-dark.json")
+output_file_path = os.path.join(script_dir, "color-fatigue.tmTheme")
+
+# Load the JSON theme file and process it
+with open(json_file_path, "r") as f:
+    json_str = f.read()
+    json_str_no_comments = clean_json(json_str)
+    theme_json = json.loads(json_str_no_comments)
+
+# Convert token colors to .tmTheme settings
+for token in theme_json["tokenColors"]:
+    scope = token["scope"]
+    if isinstance(scope, list):  # if "scope" contain an array of strings
+        # join the strings in the list into a single comma-separated string
+        scope = ", ".join(scope)
+    settings = {"scope": scope, "settings": token["settings"]}
+    if "name" in token:  # add name field if exist
+        settings["name"] = token["name"]
+    theme_plist["settings"].append(settings)
+
+# Save the .tmTheme file
+with open(output_file_path, "wb") as f:
+    plistlib.dump(theme_plist, f)
---
+++ themes/color-palette.md
@@ -0,0 +1,89 @@
+## accent
+
+| accent  | interface     | accent dim | syntax                                      |
+| ------- | ------------- | ---------- | ------------------------------------------- |
+| #faf5eb |               | #f7f4ed    |                                             |
+| #dad5c8 |               | #d6d3cc    |                                             |
+| #c0baad |               | #bdbab2    |                                             |
+| #b3ad9f | info, changed | #b4b0a7    | constant                                    |
+| #a69f91 |               | #a19d97    |                                             |
+| #9a9487 |               | #97948c    |                                             |
+| #8e897d |               | #8e8b85    | func-call, var-other, md-quote, JSX bracket |
+| #7f7b70 |               | #7b7974    |                                             |
+| #706c62 |               | #6c6a65    |                                             |
+
+---
+
+## syntax
+
+#eaeaea // type, class-lib, const-deSat
+#dcdcdc // tag-name, css-func,
+#cccccc
+#c0c0c0 // func-name, class-inherited, tag-ID, css-value
+#b3b3b3
+#aaaaaa // separator, regex-escape, md-raw
+#9d9d9d // keyword, var-lang, tag-attr, md-heading, JSON
+#8e8e8e
+#878787 // class, pkg, var, param, num, css-pseudo+prop, js-temp-str
+
+#7f7f7f
+#707070 // string, css-string, link, regex
+#686868 // comment-emphasis, css-unit,
+#5d5d5d // md-separator, embedded-string
+#555555 // comment, md-punctuation
+#4e4e4e
+#474747
+#414141
+
+---
+
+## interface
+
+#3a3a3a
+#353535
+#303030
+#2a2a2a
+#242424
+#1e1e1e
+#191919
+#151515
+#101010
+
+---
+
+## terminal
+
+- gogh-apprentice
+- ref: http://gogh-co.github.io/Gogh/
+- normal: https://coolors.co/b3ad9f-af5f5f-e0ca9d-5f875f-5f87af-5f5f87-5f8787
+- bright: https://coolors.co/dad5c8-de7878-f5dcab-87af87-8fafd7-8c8cbd-79b8b8
+
+#b3b3b3 (fg)
+#191919 (bg)
+#eaeaea (cursor)
+#303030 (selection)
+
+black: #474747 #5d5d5d
+white: #dad5c8 #faf5eb
+
+### 2024-08
+
+orange: #c8a492 #dcb5a5
+
+red: #b07878 #cc9393
+green: #778777 #9bb09b
+yellow: #d6caab #ebd6b7
+blue: #7d96ad #9db2cf
+magenta: #797994 #9f9fbd
+cyan: #769494 #92b3b3
+
+### 2024-07
+
+orange: #ad7c58 #ddac89
+
+red: #af5f5f #de7878
+green: #5f875f #87af87
+yellow: #e0bf8b #f5dcab
+blue: #5f87af #8fafd7
+magenta: #5f5f87 #8c8cbd
+cyan: #5f8787 #79b8b8
